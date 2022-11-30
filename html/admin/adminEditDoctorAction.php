7483685381a60868aaf7a4450dcf6196919a2080
edwardsung4217 <edwardsung4217@gmail.com>
2022-11-29 17:24:50 -0800

    edit functionality in place
    still no css

--- html/admin/adminEditDoctorAction.php
+++ html/admin/adminEditDoctorAction.php
@@ -5,9 +5,10 @@ $email = $_POST['email'];
 $userName = $_POST['username'];
 $password = $_POST['password'];
 $id = $_POST['Key'];
+$conn = new mysqli("localhost", "root", "", "pregnancy");
 
-//$conn = new mysqli("localhost", "root", "", "pregnancy");
-//$sql = "INSERT INTO `Doctors`(`id`, `email`, `password`, `firstName`, `lastName`, `username`) VALUES ('0','$email','$password','$firstName' , '$lastName' ,'$username')";
-//$result = $conn->query($sql);
-echo $id;
+$sql = "UPDATE `Doctors` SET `email` = '$email', `password` = '$password', `firstName` = '$firstName', `lastName` = '$lastName', `username` = '$userName' WHERE `Doctors`.`id` = $id";
+$conn->query($sql);
+echo "Database has been changed";
 ?>
+
