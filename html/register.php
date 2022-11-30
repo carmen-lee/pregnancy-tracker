ff810675b7943e95c7c8304a67dd22a0d43ab030
yaaacii <84832971+yaaacii@users.noreply.github.com>
2022-11-29 22:45:49 -0800

    Update: register connection to DB
    Connected Register Page to DB

--- html/register.php
+++ html/register.php
@@ -1,44 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-if ($_POST['inputUserName'] == "No") {
-  // think of scenario when creating a user could be an error
-  // date is in the future
-  header("Location: register.php? err= Could not create user");
-} else {
-  header("Location: login.php? registerSuccess= User created $roles");
-}
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../css/login.css?v=<?php echo time(); ?>">
+  <title>Register User</title>
+</head>
 
-/* FROM CLASS*/
+<body>
 
-$conn = mysqli_connect('localhost', 'root', '', 'pregnancy');
-$username = $_POST['inputUserName'];
-$password = $_POST['inputPassword'];
-$first = $_POST['inputFirst'];
-$last = $_POST['inputLast'];
-$userType = $_POST['inputRole'];
-$email = $_POST['inputEmail'];
-$phone = $_POST['inputPhoneNumber'];
-$birthdate = $_POST['inputBirthdate'];
+  <div class="col-md-6">
+    <div class="login-box">
+      <fieldset>
+        <h1>Register new user</h1>
+        <br>
+        <form name="frmContact" method="post" action="registerAction.php">
+          <p>
+            <label for="username">Username </label>
+            <input type="text" name="inputUserName" id="inputUserName" required>
+          </p>
+          <p>
+            <label for="password">Password</label>
+            <input type="password" name="inputPassword" id="inputPassword" required>
+          </p>
+          <p>
+            <label for="first_name">First Name</label>
+            <input type="text" name="inputFirst" id="inputFirst" required>
+          </p>
+          <p>
+            <label for="last_name">Last Name</label>
+            <input type="text" name="inputLast" id="inputLast" required>
+          </p>
+          <p>
+            <!-- <label class="radio-inline">
+                  <input type="radio" name="loginType" required value="admin" checked> Administrator
+              </label>
+              <label class="radio-inline">
+                  <input type="radio" name="loginType" required value="doctor"> Doctor
+              </label>
+              <label class="radio-inline">
+                  <input type="radio" name="loginType" required value="patient"> Patient
+              </label> -->
+            <label for="type">Type</label>
+            <select name="inputRole" id="inputRole" required>
+              <option value="administrator">Administrator</option>
+              <option value="doctor" disabled>Doctor</option>
+              <option value="patient">Patient</option>
+            </select>
+          </p>
+          <p>
+            <label for="email">Email</label>
+            <input type="email" name="inputEmail" id="inputEmail" required>
+          </p>
+          <p>
+            <label for="phoneNumber">Phone Number</label>
+            <input type="tel" name="inputPhoneNumber" placeholder="123-456-7890" id="inputPhoneNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+          </p>
+          <p>
+            <label for="birthdate">Birthdate</label>
+            <input type="date" name="inputBirthdate" id="inputBirthdate" required>
+          </p>
+          <p>
+            <?php
+            if (isset($_GET['err'])) {
+              echo '<p style="color: red;">', $_GET['err'], '</p>';
+            }
+            if (isset($_GET['success'])) {
+              echo '<p style="color: green;">', $_GET['success'], '</p>';
+              // sleep(1);
+              header("Location:login.php");
+            }
+            ?>
+          </p>
+          <p>
+            <input type="submit" name="Submit" id="registerBtn" value="Register" onsubmit="handleSubmit(e)">
+          </p>
+          <p><a href="login.php">Back to Login</a></p>
+        </form>
+      </fieldset>
+    </div>
+  </div>
+  <div class="col-md-6 login-splash"></div>
 
-if ($userType == 'patient') {
-  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, roleType, username) VALUES ('$first', '$last','$email','$password', 'Patient', '$username');";
-} else {
-  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, roleType, username) VALUES ('$first', '$last','$email','$password', 'Admin', '$username');";
-}
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <!-- JS -->
+  <!-- <script src="../js/register.js"></script> -->
+</body>
 
-
-if ($conn->query($sql)) {
-
-
-  session_start();
-  $_SESSION['username'] = $username;
-  // $_SESSION['userType'] = 'USER';
-  // $_SESSION['eid'] = $empID;
-
-  header('location:login.php');
-} else {
-  echo $conn->error;
-}
-
-
-?>
+</html>
--- html/registerAction.php
+++ html/registerAction.php
@@ -2,32 +2,43 @@
 <?php
 if ($_POST['inputUserName'] == "No") {
   // think of scenario when creating a user could be an error
-    // date is in the future
+  // date is in the future
   header("Location: register.php? err= Could not create user");
+} else {
+  header("Location: login.php? registerSuccess= User created $roles");
 }
-else {
-  header("Location: login.php? registerSuccess= User created");
+
+/* FROM CLASS*/
+
+$conn = mysqli_connect('localhost', 'root', '', 'pregnancy');
+$username = $_POST['inputUserName'];
+$password = $_POST['inputPassword'];
+$first = $_POST['inputFirst'];
+$last = $_POST['inputLast'];
+$userType = $_POST['inputRole'];
+$email = $_POST['inputEmail'];
+$phone = $_POST['inputPhoneNumber'];
+$birthdate = $_POST['inputBirthdate'];
+
+if ($userType == 'patient') {
+  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, roleType, username) VALUES ('$first', '$last','$email','$password', 'Patient', '$username');";
+} else {
+  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, roleType, username) VALUES ('$first', '$last','$email','$password', 'Admin', '$username');";
 }
 
-/* FROM CLASS */
 
-$conn = mysqli_connect('localhost', 'admin', '', 'pregnancy');
-$username = $_POST['username'];
-$password = $_POST['password'];
-$empID = $_POST['empID'];
+if ($conn->query($sql)) {
 
-$sql = "insert into USERS () values ('$username','$password','USER',$empID)";
 
-if($conn->query($sql)){
+  session_start();
+  $_SESSION['username'] = $username;
+  // $_SESSION['userType'] = 'USER';
+  // $_SESSION['eid'] = $empID;
 
-    session_start();
-    $_SESSION['username'] = $username;
-    $_SESSION['role'] = 'USER';
-    $_SESSION['eid'] = $empID;
-
-    header('location:homepage.php');
-}else{
-    echo $conn->error;
+  header('location:login.php');
+} else {
+  echo $conn->error;
 }
+
 
 ?>
