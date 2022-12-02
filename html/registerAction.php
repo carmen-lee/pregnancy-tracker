3a10d1220a6f2a7206edd4b5dbb3992018c94c80
yaaacii <84832971+yaaacii@users.noreply.github.com>
2022-12-01 20:40:30 -0800

    Auto assign doc on Register Done

--- html/registerAction.php
+++ html/registerAction.php
@@ -9,7 +9,6 @@
 // }
 
 /* FROM CLASS*/
-
 $conn = mysqli_connect('localhost', 'root', '', 'pregnancy');
 $username = $_POST['inputUserName'];
 $password = $_POST['inputPassword'];
@@ -20,16 +19,24 @@ $email = $_POST['inputEmail'];
 $phone = $_POST['inputPhoneNumber'];
 $birthdate = $_POST['inputBirthdate'];
 
+$sqln = "SELECT assigned_doctorId FROM Users WHERE assigned_doctorId !='NULL' GROUP BY assigned_doctorId ORDER BY COUNT(assigned_doctorId) ASC LIMIT 1";
+// $sqln = "INSERT INTO Users(first_name, last_name, email, userpassword, role, username, birthdate, phone, assigned_doctorId) VALUES ('$first', '$last','$email','$password', 'Patient', '$username', '$birthdate', '$phone', '$docId');";
+
+$docID = mysqli_query($conn, $sqln);
+$row = mysqli_fetch_assoc($docID);
+$docId = $row['assigned_doctorId'];
+
+// header("Location: register.php? err= $conn->error");
+
 if ($userType == 'patient') {
-  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, role, username, birthdate, phone) VALUES ('$first', '$last','$email','$password', 'Patient', '$username', '$birthdate', '$phone');";
+  $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, role, username, birthdate, phone, assigned_doctorId) VALUES ('$first', '$last','$email','$password', 'Patient', '$username', '$birthdate', '$phone', '$docId');";
 } else {
   $sql = "INSERT INTO Users(first_name, last_name, email, userpassword, role, username, birthdate, phone) VALUES ('$first', '$last','$email','$password', 'Admin', '$username', '$birthdate', '$phone');";
 }
 
+// $conn->close();
 
 if ($conn->query($sql)) {
-
-
   session_start();
   $_SESSION['username'] = $username;
   // $_SESSION['userType'] = 'USER';
