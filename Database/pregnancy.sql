6d223744cb860d0ade885dd373e0400533ea1740
Carmen Lee <carmen.lee.lv@gmail.com>
2022-12-05 14:11:41 -0800

    updated db

--- Database/pregnancy.sql
+++ Database/pregnancy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2022 at 01:15 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Dec 05, 2022 at 11:10 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,25 +31,38 @@ CREATE TABLE `appointments` (
   `id` int(11) UNSIGNED NOT NULL,
   `apptDate` date NOT NULL,
   `apptTime` time NOT NULL,
-  `status` enum('COMPLETED','REQUESTED','SCHEDULED','CANCELLED') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'REQUESTED',
+  `status` enum('COMPLETED','REQUESTED','SCHEDULED','CANCELLED') CHARACTER SET utf8 NOT NULL DEFAULT 'REQUESTED',
   `patientId` int(11) NOT NULL,
   `doctorId` int(11) NOT NULL,
   `reason` varchar(255) NOT NULL DEFAULT 'No reason provided'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `appointments`
 --
 
 INSERT INTO `appointments` (`id`, `apptDate`, `apptTime`, `status`, `patientId`, `doctorId`, `reason`) VALUES
-(1, '2022-01-01', '10:00:00', 'COMPLETED', 3, 2, 'No reason provided'),
-(2, '2022-12-01', '10:00:00', 'REQUESTED', 3, 2, 'No reason provided'),
-(3, '2022-10-01', '10:00:00', 'CANCELLED', 3, 2, 'No reason provided'),
-(4, '2022-11-01', '10:00:00', 'SCHEDULED', 3, 2, 'No reason provided'),
-(5, '2022-11-01', '15:25:00', 'REQUESTED', 3, 2, 'dumb'),
-(6, '2022-11-30', '18:33:00', 'SCHEDULED', 4, 2, ''),
-(7, '2022-11-10', '15:37:00', 'SCHEDULED', 3, 2, ''),
-(8, '2022-11-10', '15:40:00', 'SCHEDULED', 4, 2, 'test');
+(1, '2022-04-15', '10:00:00', 'COMPLETED', 4, 2, 'Lectus sit amet est placerat.'),
+(2, '2022-07-10', '12:15:00', 'COMPLETED', 4, 2, 'Adipiscing commodo elit at imperdiet dui accumsan'),
+(3, '2022-07-24', '10:30:00', 'COMPLETED', 5, 3, 'Adipiscing elit duis tristique sollicitudin nibh sit amet'),
+(4, '2022-08-29', '11:00:00', 'COMPLETED', 5, 3, 'Nisl nisi scelerisque eu ultrices vitae auctor eu.'),
+(5, '2022-10-24', '10:00:00', 'COMPLETED', 6, 2, 'Elementum nibh tellus molestie nunc non blandit massa enim nec.'),
+(6, '2022-10-24', '14:00:00', 'COMPLETED', 6, 2, 'Sollicitudin tempor id eu nisl nunc mi. Orci a scelerisque purus semper eget.'),
+(7, '2022-01-11', '11:00:00', 'COMPLETED', 7, 3, 'Tempor nec feugiat nisl pretium fusce id velit ut tortor.'),
+(8, '2022-01-18', '10:00:00', 'COMPLETED', 7, 3, 'Purus sit amet volutpat consequat mauris nunc congue.'),
+(9, '2022-04-12', '14:15:00', 'COMPLETED', 4, 2, 'Imperdiet proin fermentum leo vel orci porta non pulvinar neque.'),
+(10, '2023-01-19', '10:00:00', 'SCHEDULED', 4, 2, 'Lectus sit amet est placerat.'),
+(12, '2023-04-19', '10:30:00', 'SCHEDULED', 5, 3, 'Adipiscing elit duis tristique sollicitudin nibh sit amet'),
+(14, '2023-02-14', '10:00:00', 'SCHEDULED', 6, 2, 'Elementum nibh tellus molestie nunc non blandit massa enim nec.'),
+(16, '2023-01-21', '11:00:00', 'SCHEDULED', 7, 3, 'Tempor nec feugiat nisl pretium fusce id velit ut tortor.'),
+(17, '2023-01-19', '12:45:00', 'REQUESTED', 4, 2, 'Lectus sit amet est placerat.'),
+(18, '2023-04-19', '11:45:00', 'REQUESTED', 5, 3, 'Adipiscing elit duis tristique sollicitudin nibh sit amet'),
+(19, '2023-02-14', '12:30:00', 'REQUESTED', 6, 2, 'Elementum nibh tellus molestie nunc non blandit massa enim nec.'),
+(20, '2023-01-21', '09:45:00', 'REQUESTED', 7, 3, 'Tempor nec feugiat nisl pretium fusce id velit ut tortor.'),
+(21, '2022-07-24', '12:45:00', 'CANCELLED', 4, 2, 'Lectus sit amet est placerat.'),
+(22, '2022-07-15', '11:45:00', 'CANCELLED', 5, 3, 'Adipiscing elit duis tristique sollicitudin nibh sit amet'),
+(23, '2022-09-08', '12:30:00', 'CANCELLED', 6, 2, 'Elementum nibh tellus molestie nunc non blandit massa enim nec.'),
+(24, '2022-11-11', '09:45:00', 'CANCELLED', 7, 3, 'Tempor nec feugiat nisl pretium fusce id velit ut tortor.');
 
 -- --------------------------------------------------------
 
@@ -64,15 +77,27 @@ CREATE TABLE `medication` (
   `medDosage` varchar(255) DEFAULT NULL,
   `medFrequency` varchar(255) DEFAULT NULL,
   `medFood` enum('With Food','Without Food') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `medication`
 --
 
 INSERT INTO `medication` (`increment`, `patientID`, `medName`, `medDosage`, `medFrequency`, `medFood`) VALUES
-(2, 4, 'er', 'hjk', 'j', 'With Food'),
-(3, 3, 'Pain meds', '500mg', '2 times a day', 'With Food');
+(1, 4, 'Antalamin', '10mg', '2 times a day', 'With Food'),
+(2, 5, 'Antalamin', '10mg', '2 times a day', 'With Food'),
+(3, 6, 'Antalamin', '10mg', '2 times a day', 'With Food'),
+(4, 7, 'Antalamin', '10mg', '2 times a day', 'With Food'),
+(5, 4, 'Sensitamine', '20mg', '2 times a day', 'Without Food'),
+(6, 5, 'Thyroxitrol Naloxacin', '15mg', '2 times a day', 'Without Food'),
+(7, 6, 'Sensitamine', '15mg', '2 times a day', 'Without Food'),
+(8, 7, 'Alglucoderal', '15mg', '2 times a day', 'Without Food'),
+(9, 4, 'Thyroxitrol Naloxacin', '10mg', '2 times a day', 'Without Food'),
+(10, 5, 'Tamoletra Betavax', '20mg', '2 times a day', 'Without Food'),
+(11, 4, 'Phytocerol', '10mg', '2 times a day', 'With Food'),
+(12, 5, 'Alglucoderal', '10mg', '2 times a day', 'With Food'),
+(13, 6, 'Oloferal Unilac', '20mg', '2 times a day', 'With Food'),
+(14, 7, 'Symbysteride', '15mg', '2 times a day', 'With Food');
 
 -- --------------------------------------------------------
 
@@ -83,23 +108,27 @@ INSERT INTO `medication` (`increment`, `patientID`, `medName`, `medDosage`, `med
 CREATE TABLE `pregnancies` (
   `id` int(11) NOT NULL,
   `doctor_id` int(11) DEFAULT NULL,
-  `momHealth` varchar(255) DEFAULT NULL,
-  `Health` varchar(255) DEFAULT NULL,
+  `momHealth` enum('Healthy','Unhealthy') DEFAULT NULL,
+  `Health` enum('Healthy','Unhealthy') DEFAULT NULL,
   `due_date` date DEFAULT NULL,
-  `status` enum('CURRENT','PAST') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'CURRENT',
+  `status` enum('CURRENT','PAST') CHARACTER SET utf8 NOT NULL DEFAULT 'CURRENT',
   `patientID` int(11) NOT NULL,
   `babyName` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pregnancies`
 --
 
 INSERT INTO `pregnancies` (`id`, `doctor_id`, `momHealth`, `Health`, `due_date`, `status`, `patientID`, `babyName`) VALUES
-(2, NULL, 'Healthy-ish', 'Healthy', '2020-02-05', 'PAST', 3, 'Johnny Jr Jr.'),
-(6, 2, 'its good', 'Its growing', '2023-06-22', 'CURRENT', 3, 'CHlid32'),
-(7, 2, 'its good', 'Healthy-ish', '2019-06-05', 'PAST', 4, 'Child#64'),
-(8, 2, 'Healthy-ish', 'yes', '2023-06-01', 'CURRENT', 4, 'CHlid32');
+(1, 2, 'Healthy', 'Healthy', '2023-01-25', 'CURRENT', 4, 'Liam'),
+(2, 2, 'Healthy', 'Healthy', '2023-05-26', 'CURRENT', 5, 'Noah'),
+(3, 3, 'Healthy', 'Healthy', '2023-06-02', 'CURRENT', 6, 'Olivia'),
+(4, 3, 'Healthy', 'Healthy', '2023-07-19', 'CURRENT', 7, 'Emma'),
+(5, 2, 'Healthy', 'Healthy', '2021-03-28', 'PAST', 4, 'Charlotte'),
+(6, 2, 'Healthy', 'Healthy', '2021-11-08', 'PAST', 5, 'Ava'),
+(7, 3, 'Unhealthy', 'Healthy', '2021-06-01', 'PAST', 6, 'Sophia'),
+(8, 2, 'Unhealthy', 'Healthy', '2021-03-12', 'PAST', 7, 'James');
 
 -- --------------------------------------------------------
 
@@ -122,17 +151,20 @@ CREATE TABLE `Users` (
   `emerCon_name` varchar(255) DEFAULT NULL,
   `emerCon_phone` varchar(255) DEFAULT NULL,
   `emerCon_relation` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `Users`
 --
 
 INSERT INTO `Users` (`id`, `first_name`, `last_name`, `email`, `userpassword`, `role`, `username`, `assigned_doctorId`, `birthdate`, `phone`, `address`, `emerCon_name`, `emerCon_phone`, `emerCon_relation`) VALUES
-(1, 'Admin', 'Istrator', 'admin@example.com', 'password', 'ADMIN', 'admin', NULL, NULL, NULL, NULL, NULL, '', NULL),
-(2, 'Johnny', 'Appleseed', 'JohnnyAppleseed@example.com', 'password', 'DOCTOR', 'doctor', NULL, NULL, NULL, NULL, NULL, '', NULL),
-(3, 'Jane', 'Doe', 'JaneDoe@example.com', 'password', 'PATIENT', 'patient', 2, NULL, '702-338-1379', NULL, NULL, '', NULL),
-(4, 'Cici', 'Siu', 'starxdevilx@gmail.com', 'Password1!', 'PATIENT', 'cici', 2, '2022-11-10', '123-123-1234', 'unlvsaddy', 'hi', '123-132-1234', 'whoknows');
+(1, 'Admin', 'User', 'admin@example.com', 'password', 'ADMIN', 'admin', NULL, NULL, NULL, NULL, NULL, '', NULL),
+(2, 'Anthony', 'Fauci', 'afauci@example.com', 'password', 'DOCTOR', 'afauci', NULL, NULL, NULL, NULL, NULL, '', NULL),
+(3, 'Mehmet', 'Oz', 'moz@example.com', 'password', 'DOCTOR', 'moz', NULL, NULL, NULL, NULL, NULL, '', NULL),
+(4, 'Elaine', 'Joseph', 'ejoseph@example.com', 'password', 'PATIENT', 'ejoseph', 2, '1979-04-05', '401-864-0745', '65 Linda Avenue Taylors, SC 29687', 'Santiago Bass', '978-660-4036', 'Husband'),
+(5, 'Velma', 'Warner', 'vwarner@example.com', 'password', 'PATIENT', 'vwarner', 3, '1979-06-11', '631-317-8283', '8238 Birch Hill Avenue Montclair, NJ 07042', 'Kirk Boyd', '774-563-2921', 'Husband'),
+(6, 'Rosemary', 'Matthews', 'rmatthews@example.com', 'password', 'PATIENT', 'rmatthews', 2, '1984-04-20', '205-393-2646', '596 Franklin St. Irmo, SC 29063', 'Owen Curtis', '210-722-3523', 'Husband'),
+(7, 'Tracy', 'Hammond', 'thammond@example.com', 'password', 'PATIENT', 'thammond', 3, '1992-05-16', '515-291-6664', '69 Magnolia Street Parlin, NJ 08859', 'Isaac Harmon', '386-233-9751', 'Husband');
 
 --
 -- Indexes for dumped tables
@@ -171,13 +203,13 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `medication`
 --
 ALTER TABLE `medication`
-  MODIFY `increment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `increment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `pregnancies`
@@ -189,7 +221,7 @@ ALTER TABLE `pregnancies`
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
